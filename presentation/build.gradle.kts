5aeaa0b1bf771b5b5b1aeb73885f7b79a9896575
ZEIAD-LAPTOP\zeiad <zazaoskar928@gmail.com>
2026-06-27 18:20:59 +0300

    Setup Jetpack Compose in presentation module, 1- update build.gradle.kts with Compose dependencies and features 2- add default Compose theme colors 3- ignore .idea folder in .gitignore

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -20,12 +21,22 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
