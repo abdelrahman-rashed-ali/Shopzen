6b30fbd3f6d3afb21c1e2ab16d5a2babe1374365
yusefellban <ellbanyousef@gmail.com>
2026-06-27 14:33:28 +0300

    setup multi-module architecture and configure Gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.android.library) apply false
+    //yousef
+    //yousef
+    //rashed
+    //rashed
+    //nour
+    //nour
+    //ziad
+    //ziad
 }
