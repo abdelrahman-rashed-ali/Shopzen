172fb161f17590cc1a588dc3bf1e466224440324
ZEIAD-LAPTOP\zeiad <zazaoskar928@gmail.com>
2026-06-29 16:30:21 +0300

    follow up/ update TEST directories, remove missed old java directories from domain and presentation

--- domain/bin/main/iti/domain/Auth/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.shopzen.domain.auth.model
-
-data class User(
-    val uid: String,
-    val email: String,
-    val displayName: String,
-    val photoUrl: String?,
-    val isEmailVerified: Boolean
-)
