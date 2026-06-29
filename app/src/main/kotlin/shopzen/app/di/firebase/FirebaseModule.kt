81b6423aea1555fb6600a90dbc1a621d4a43cfa2
Noureldeen Osama Mohamed <138819436+Noureldeen75@users.noreply.github.com>
2026-06-29 22:33:05 +0300

    Restore core Ktor network modules and configure standalone FirebaseModule DI

---
+++ app/src/main/kotlin/shopzen/app/di/firebase/FirebaseModule.kt
@@ -0,0 +1,32 @@
+package shopzen.app.di.firebase
+
+import com.google.firebase.auth.FirebaseAuth
+// import shopzen.data.auth.remote.RemoteAuthDataSource
+// import shopzen.data.auth.remote.RemoteAuthDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class FirebaseModule {
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideFirebaseAuth(): FirebaseAuth {
+            return FirebaseAuth.getInstance()
+        }
+
+    }
+
+    // @Binds
+    // @Singleton
+    // abstract fun bindRemoteAuthDataSource(
+    //     impl: RemoteAuthDataSourceImpl
+    // ): RemoteAuthDataSource
+}
